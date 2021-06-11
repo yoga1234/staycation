d50e677cff76591f043c392652a75ffc5381d243
Septiana Yoga <septianayoga30@gmail.com>
2021-06-11 20:26:39 +0700

    update
    Update and finishin header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react"
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import "assets/scss/style.scss"
-import LandingPage from "pages/LandingPage"
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return <div className="App">
