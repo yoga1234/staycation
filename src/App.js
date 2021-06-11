67f5d70b4a0b54e42e76c200e4b1b133ac1906de
Septiana Yoga <septianayoga30@gmail.com>
2021-06-11 15:00:05 +0700

    edit
    Editing style for header and App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import 'assets/scss/style.scss'
+import "assets/scss/style.scss"
+import LandingPage from "pages/LandingPage"
 
 function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+  return <div className="App">
+    <Router>
+      <Route path="/" component={LandingPage}></Route>
+    </Router>
+  </div>
 }
 
-export default App;
+export default App
