a34b681d7bb0a3cd03b8f0ac319326685c160b01
Septiana Yoga <septianayoga30@gmail.com>
2021-06-15 14:07:38 +0700

    Update Hero.js

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import ImageHero from 'assets/images/img-hero.jpg'
-import Imagehero_ from 'assets/images/img-hero-frame.jpg'
+import ImageHero_ from 'assets/images/img-hero-frame.jpg'
 import IconCities from 'assets/images/icons/icon_cities.svg'
 import IconTraveler from 'assets/images/icons/icon_traveler.svg'
 import IconTreasure from 'assets/images/icons/icon_treasure.svg'
@@ -19,7 +19,7 @@ export default function Hero(props) {
   return (
     <section className="container pt-4">
       <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 422 }}>
+        <div className="col-auto pr-5" style={{ width: 530 }}>
           <h1 className="h2 font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br />
             Start Next Vacation
@@ -32,7 +32,7 @@ export default function Hero(props) {
           </Button>
 
           <div className="row mt-5">
-            <div className="col-auto">
+            <div className="col-auto" style={{ marginRight: 50}}>
               <img
                 width="36"
                 height="36" 
@@ -43,7 +43,7 @@ export default function Hero(props) {
                 {props.data.travelers} <span className="text-gray-500 font-weight-light">Travelers</span>
               </h6>
             </div>
-            <div className="col-auto">
+            <div className="col-auto" style={{ marginRight: 35}}>
               <img
                 width="36"
                 height="36" 
@@ -54,7 +54,7 @@ export default function Hero(props) {
                 {props.data.treasures} <span className="text-gray-500 font-weight-light">Treasures</span>
               </h6>
             </div>
-            <div className="col-auto">
+            <div className="col-auto" style={{ marginRight: 35}}>
               <img
                 width="36"
                 height="36" 
@@ -65,6 +65,13 @@ export default function Hero(props) {
                 {props.data.cities} <span className="text-gray-500 font-weight-light">Cities</span>
               </h6>
             </div>
+          </div>
+        </div>
+
+        <div className="col-6 pl-5">
+          <div style={{ width: 520, height: 410}}>
+            <img src={ImageHero} alt="Room with couches" className="img-fluid position-absolute" style={{ margin: '-30px 0 0 -30px', zIndex: 1 }} />
+            <img src={ImageHero_} alt="Room with couches frame" className="img-fluid position-absolute" style={{ margin: '0 -15px -15px 0' }} />
           </div>
         </div>
       </div>
