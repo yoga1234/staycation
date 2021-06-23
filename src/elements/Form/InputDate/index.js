185cd0bc01c04ed4b656e76a080332a52e664424
Septiana Yoga <septianayoga30@gmail.com>
2021-06-23 16:44:05 +0700

    create initial code for formdate

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from "React";
+import propTypes from "prop-types";
+
+import { DateRange } from "react-date-range";
+
+import "./index.scss";
+import "react-date-range/dist/style.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import formatDate from "utils/formatDate"
+import iconCalendar from "assets/images/ic_calendar.svg"
