8b89c2c4b9b25d984e5d29f328c5658c21c83eff
Septiana Yoga <septianayoga30@gmail.com>
2021-06-23 14:59:29 +0700

    adding test, renaming and editing error on button.

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -5,9 +5,10 @@ import propTypes from "prop-types";
 import "./index.scss";
 
 export default function Number(props) {
-  const { value, placeholder, name, min, max, prefix, suffix } = props;
+  const { value, placeholder, name, min, max, prefix, suffix, isSuffixPlural } =
+    props;
 
-  const { InputValue, setInputValue } = useState(`${prefix}${value}${suffix}`);
+  const [InputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
 
   const onChange = (e) => {
     let value = String(e.target.value);
@@ -24,7 +25,9 @@ export default function Number(props) {
           value: +value,
         },
       });
-      setInputValue(`${prefix}${value}${suffix}`);
+      setInputValue(
+        `${prefix}${value}${suffix}${isSuffixPlural  && value > 1 ? "s" : ""}`
+      );
     }
   };
 
@@ -84,6 +87,7 @@ Number.defaultProps = {
 Number.propTypes = {
   value: propTypes.oneOfType([propTypes.string, propTypes.number]),
   onChange: propTypes.func,
+  isSuffixPlural: propTypes.bool,
   placeholder: propTypes.string,
   outerClassName: propTypes.string,
 };
