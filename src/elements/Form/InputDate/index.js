b8d915bae5bbcaa852b176e1fc2f0af031ccb5a6
Septiana Yoga <septianayoga30@gmail.com>
2021-06-25 15:56:39 +0700

    form testing
    add form date testing and add library for date.

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -1,17 +1,14 @@
-// this import file is incomplete
-// please read the requirement file
-
-import React, { useState, useRef, useEffect } from "React";
+import React, { useState, useRef, useEffect } from "react";
 import propTypes from "prop-types";
 
 import { DateRange } from "react-date-range";
 
 import "./index.scss";
-import "react-date-range/dist/style.css"; // main css file
+import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
 import formatDate from "utils/formatDate";
-import iconCalendar from "assets/images/ic_calendar.svg";
+import iconCalendar from "assets/images/icons/icon-calendar.svg";
 
 export default function Date(props) {
   const { value, placeholder, name } = props;
