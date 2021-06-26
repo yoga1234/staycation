7cf3a628a5cb4b0db3d64b6d475a4ce238d28faf
Septiana Yoga <septianayoga30@gmail.com>
2021-06-26 12:10:41 +0700

    updating for details page.

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -3,9 +3,6 @@ import Fade from "react-reveal/Fade";
 
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHero_ from "assets/images/img-hero-frame.jpg";
-import IconCities from "assets/images/icons/icon_cities.svg";
-import IconTraveler from "assets/images/icons/icon_traveler.svg";
-import IconTreasure from "assets/images/icons/icon_treasure.svg";
 
 import Button from "elements/Button";
 
@@ -49,7 +46,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTraveler}
+                  src="/images/icon_traveler.svg"
                   alt={`${props.data.travelers} Travelers`}
                 />
                 <h6 className="mt-3">
@@ -63,7 +60,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTreasure}
+                  src="/images/icon_treasure.svg"
                   alt={`${props.data.treasures} Treasures`}
                 />
                 <h6 className="mt-3">
@@ -77,7 +74,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconCities}
+                  src="/images/icon_cities.svg"
                   alt={`${props.data.cities} Cities`}
                 />
                 <h6 className="mt-3">
