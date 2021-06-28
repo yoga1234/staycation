16923115d8a26b736e4e6fd2b077503f22c7d829
Septiana Yoga <septianayoga30@gmail.com>
2021-06-28 11:01:20 +0700

    edit header
    Edit header for checkout page using centered header.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 
 function App() {
   return <div className="App">
     <Router>
       <Route exact path="/" component={LandingPage}></Route>
       <Route exact path="/properties/:id" component={DetailsPage}></Route>
+      <Route path="/checkout" component={Checkout} />
     </Router>
   </div>
 }
