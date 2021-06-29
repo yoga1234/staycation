49a616d390d6d9b2c354f729e543d457d8ecbeca
Septiana Yoga <septianayoga30@gmail.com>
2021-06-29 14:05:55 +0700

    adding payment component

---
+++ src/parts/Checkout/Payment.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+import InputText from "elements/Form/InputText";
+import InputFile from "elements/Form/InputFile";
+
+import logoBca from "assets/images/logo-bca.jpg";
+import logoMandiri from "assets/images/logo-mandiri.jpg";
+
+export default function Payment(props) {
+  const { data, ItemDetails, checkout } = props;
+
+  const tax = 10;
+  const subTotal = ItemDetails.price * checkout.duration;
+  const grandTotal = ((subTotal * tax) / 100) * subTotal;
+
+  return (
+    <Fade>
+      <div className="container" style={{ marginBottom: 30 }}>
+        <div className="row justify-content-center align-items center">
+          <div className="col-5 border-right py-5" style={{ paddingRight: 80 }}>
+            <Fade delay={300}>
+              <p className="mb-4">Transfer Pembayaran:</p>
+              <p>Tax: {tax}%</p>
+              <p>Sub total: ${subTotal} USD</p>
+              <p>Total: ${grandTotal} USD</p>
+              <div className="row mt-4">
+                <div className="col-3 text-right">
+                  <img src={logoBca} alt="Bank Central Asia" width="60" />
+                </div>
+                <div className="col">
+                  <dt>
+                    <dd>Bank Central Asia</dd>
+                    <dd>2208 1996</dd>
+                    <dd>BuildWith Angga</dd>
+                  </dt>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-3 text-right">
+                  <img src={logoMandiri} alt="Mandiri" width="60" />
+                </div>
+                <div className="col">
+                  <dt>
+                    <dd>Bank Mandiri</dd>
+                    <dd>2208 1996</dd>
+                    BuildWith Angga
+                  </dt>
+                </div>
+              </div>
+            </Fade>
+          </div>
+          <div className="col-5 py-5">
+            <Fade delay={600}>
+              <label htmlFor="proofPayment">Upload Bukti Transfer</label>
+              <InputFile
+                accept="image/*"
+                id="proofPayment"
+                name="proofPayment"
+                value={data.proofPayment}
+                onChange={props.onChange}
+              />
+
+              <label htmlFor="bankName">Asal Bank</label>
+              <InputText
+                id="bankName"
+                name="bankName"
+                type="text"
+                value={data.bankName}
+                onChange={props.onChange}
+              />
+            </Fade>
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
